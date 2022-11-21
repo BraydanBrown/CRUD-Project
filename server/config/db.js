319d11e0d2345fa178e6ecc5faef0a3f09f5b174
Braydan Brown <73270460+BraydanBrown@users.noreply.github.com>
2022-11-21 13:55:33 -0500

    modified landing page, db.js URI

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    URI: "mongodb+srv://braydan:lXtPJgTQvL8am6M0@cluster0.v4m4jlk.mongodb.net/assignments" // URI allowing connection to the database
+    URI: "mongodb+srv://group13:rK66clKjJhkpjNcY@cluster0.4rxc3f1.mongodb.net/test" // URI allowing connection to the database
 }
