9805e51677fda38d8e3c155a8143072d81f29964
Braydan Brown <73270460+BraydanBrown@users.noreply.github.com>
2022-12-15 19:42:30 -0500

    github-3

--- server/config/app.js
+++ server/config/app.js
@@ -75,16 +75,20 @@ passport.deserializeUser(function(obj, done) {
 });
 
 passport.use(new GitHubStrategy({
-  clientID: GITHUB_CLIENT_ID,
-  clientSecret: GITHUB_CLIENT_SECRET,
-  callbackURL: "http://127.0.0.1:3000/auth/github/callback"
-},
-function(accessToken, refreshToken, profile, done) {
-  process.nextTick(function () {    
-    return done(null, profile);
-  });
-}
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // process.nextTick(function () {    
+    //   return done(null, profile);
+    user.findOne({ displayName: profile.username }, function (err, user) {
+      return cb(err, user);
+    });
+  }
 ));
+
+
 
 //init passport
 app.use(passport.initialize());
